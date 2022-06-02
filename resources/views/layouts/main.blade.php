43befc2c11a2675c997930a2122308e0e161cdea
lucassimione <37031937+lucassimione@users.noreply.github.com>
2022-06-02 09:37:24 -0400

    feat: adicionando a pasta de layouts.

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+
+    <!-- fonte do google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&family=Roboto" rel="stylesheet">
+    <!-- css bootstrap !-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- css interno !-->
+    <link rel="stylesheet" href="/css/styles.css">
+    <!-- js interno !-->
+    <script src="/js/scripts.js"></script>
+</head>
+
+<body>
+    @yield('content')
+    <footer>
+        <p>HDC Events &copy; 2022</p>
+    </footer>
+</body>
+
+</html>
