9bb26b86b9ae95a334a09da26015e9ec136de198
lucassimione <37031937+lucassimione@users.noreply.github.com>
2022-06-01 21:38:51 -0400

    feat: iniciando com blade (variáveis passadas pelo controller para a view e como utilizar o php misturado com html).

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = 'Lucas Rodrigues Simione';
+    return view('welcome', ['nome' => $nome]);
 });
 
 Route::get('/contato', function () {
