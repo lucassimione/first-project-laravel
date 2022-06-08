0b2c7aa74ebce8e71f33613299e10e05c130ec9b
lucassimione <37031937+lucassimione@users.noreply.github.com>
2022-06-08 08:48:53 -0400

    adicionando uma dashboard ao projeto.

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,5 @@ Route::get('/events/{id}', [EventController::class, 'show']); // indicando qual 
 Route::post('/events', [EventController::class, 'store']); 
 
 Route::get('/contact', [ContactController::class, 'index']); // indicando qual controller será acionado a partir da requisição dessa rota
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
+
